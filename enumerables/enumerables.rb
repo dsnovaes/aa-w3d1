e61596c5a69ffb85a9c0984810a28d4faf2af5eb
Diego Novaes <dsnovaes1@gmail.com>
2022-10-17 20:51:55 -0700

    enumerables done

--- enumerables/enumerables.rb
+++ enumerables/enumerables.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class Array
     def my_each(&blk)
         i = 0
@@ -53,20 +55,40 @@ class Array
         result
     end
 
-    def my_zip(*args)
-        result = Array.new(self.length) { Array.new(args.length + 1) }
-        (0...self.length).each do |i|
-            result[i][0] = self[i]
+    def my_zip(*args)    
+        zipped = [] # creates array which will be populated and returned
+        self.length.times do |i| # iterate self.times increasing the value of i
+        #   debugger
+          subzip = [self[i]] # creates an array with the i_th value of self
+          arrays.my_each do |array| # iterates every array taken in as arguments
+            subzip << array[i] # append the i_th array into the subzip array 
+          end
+          zipped << subzip # append the subzip array into the array being returned in the end
         end
-        (0...args.length).each do |i|
-            result[i][i+1] = args[i][i]
-        end        
-
-        result
+        zipped
     end
 
     def my_rotate(times=1)
-        
+        idx = times % self.length
+        # debugger
+        self.drop(idx) + self.take(idx)
+    end
+
+
+
+    def my_join(separator="")
+        result = ""
+        self.length.times do |i| 
+            result += self[i]
+            result += separator if i != (self.length-1)
+        end
+        result
+    end
+
+    def my_reverse
+        result = []
+        self.my_each { |ele| result.unshift(ele) }
+        result
     end
 
 
@@ -79,26 +101,9 @@ p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
 p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
 
 
-a = [ 4, 5, 6 ]
-b = [ 7, 8, 9 ]
-p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
-# p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
-# [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
-
 # c = [10, 11, 12]
 # d = [13, 14, 15]
 # p [1, 2].my_zip(a, b, c, d)
 
 # p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
 
-
-
-# arr1 = [1,2,3,4,5,6]
-# arr2 = [2,4,6,8]
-# arr1.my_each { |el| p el*3 }
-# p arr1.my_select { |ele| ele.even? }
-# p arr1.my_reject { |ele| ele.even? }
-# p arr1.my_any? { |ele| ele == 7 } # false
-# p arr1.my_any? { |ele| ele.even? } # true
-# p arr1.my_all? { |ele| ele.even? } # false
-# p arr2.my_all? { |ele| ele.even? } # true
---
+++ enumerables/my_rotate.rb
@@ -0,0 +1,5 @@
+# this is a test
+
+a = [0,1,2,3,4,5,6,7,8]
+times = 2
+p a.drop(times) + a.take(times)
